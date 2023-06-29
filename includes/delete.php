375edb8ad7e7abf5be37e288eb8ecc98659e23b9
priyabista <bistapriya135@gmail.com>
2023-06-29 09:45:20 +0545

    made changes in delete query

--- includes/delete.php
+++ includes/delete.php
@@ -3,5 +3,5 @@ session_start();
 include('../dbconfig/dbconfig.php');
 $userid = $_GET['userid'];
 $q= " DELETE FROM `portals` WHERE p_id= $userid ";
-$query= mysqli_query($con, $q);
+// $query= mysqli_query($con, $q);
 ?>
